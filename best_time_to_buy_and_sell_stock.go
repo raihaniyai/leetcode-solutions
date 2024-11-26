3d3d2233b3a5d1b33ff207a4a8a3f942f497e94e
Super <super@origami.local>
2024-11-26 15:51:38 +0700

    solution: best time to buy and sell stock

---
+++ best_time_to_buy_and_sell_stock.go
@@ -0,0 +1,16 @@
+func maxProfit(prices []int) int {
+    maxProfit := 0
+    minPrice := prices[0]
+
+    for i := 1; i < len(prices); i++ {
+        if prices[i] < minPrice {
+            minPrice = prices[i]
+        }
+
+        if prices[i] - minPrice > maxProfit {
+            maxProfit = prices[i] - minPrice
+        }
+    }
+
+    return maxProfit
+}
